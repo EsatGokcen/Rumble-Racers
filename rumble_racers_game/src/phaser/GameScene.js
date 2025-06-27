7c58732b74e6a45941c805e81a1bbb108660f2ee
Esat Gokcen <esatgokcen173@gmail.com>
2025-06-27 19:54:14 +0100

    updated game scene to include new modular terrain logic

--- rumble_racers_game/src/phaser/GameScene.js
+++ rumble_racers_game/src/phaser/GameScene.js
@@ -1,5 +1,6 @@
 import Phaser from 'phaser';
 import Vehicle from './vehicle/Vehicle';
+import Terrain from './terrain/Terrain';
 
 export default class GameScene extends Phaser.Scene {
   constructor() {
@@ -9,12 +10,13 @@ export default class GameScene extends Phaser.Scene {
   create() {
     this.cameras.main.setBackgroundColor('#222');
 
-    this.add.text(100, 100, 'Hill Climb Game Started!', {
+    this.add.text(100, 100, 'Rumble Racers Game Started!', {
       font: '24px Arial',
       fill: '#ffffff'
     });
 
-    this.createHillyTerrain();
+    const terrain = new Terrain(this);
+    terrain.generateHills();
 
     // Add vehicle
     this.vehicle = new Vehicle(this, 200, 400);
@@ -22,36 +24,5 @@ export default class GameScene extends Phaser.Scene {
 
   update() {
     this.vehicle?.update();
-  }
-
-  createHillyTerrain() {
-    const startX = 0;
-    const startY = 550;
-    const segmentWidth = 80;
-    const segmentCount = 30;
-    const amplitude = 80;
-
-    const points = [];
-
-    for (let i = 0; i <= segmentCount; i++) {
-      const x = startX + i * segmentWidth;
-      const y = startY - Math.sin(i * 0.5) * amplitude;
-      points.push({ x, y });
-    }
-
-    points.push({ x: points[points.length - 1].x, y: 600 });
-    points.push({ x: points[0].x, y: 600 });
-
-    const terrain = this.matter.add.fromVertices(0, 0, points, { isStatic: true }, true);
-
-    const graphics = this.add.graphics();
-    graphics.fillStyle(0x00ff00, 1);
-    graphics.beginPath();
-    graphics.moveTo(points[0].x, points[0].y);
-    for (let i = 1; i < points.length; i++) {
-      graphics.lineTo(points[i].x, points[i].y);
-    }
-    graphics.closePath();
-    graphics.fillPath();
   }
 }
